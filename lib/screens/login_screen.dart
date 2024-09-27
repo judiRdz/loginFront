9256daa3ff70e3bba87ce2a071f6d88eb1c8de41
Judith Rodriguez <rodriguezsalinasperla@gmail.com>
2024-09-27 10:53:18 -0600

    27/09/2024

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -36,7 +36,68 @@ class _LoginForm extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Column(
-              
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(
+                  height: 16
+                ),
+                Container(
+                  width: size.width * 0.80,
+                  height: size.height * 0.17,
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(
+                    //TODO: poner una imagen
+                    //image: DecorationImage(image: ...)
+                  ),
+                ),
+                Container(
+                  width: size.width * 0.80,
+                  height: size.height * 0.05,
+                  alignment: Alignment.center,
+                ),
+                TextFormField(
+                  autocorrect: false,
+                  keyboardType: TextInputType.emailAddress,
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    hintText: 'ejemplo@correo.com',
+                    labelText: 'Email',
+                    labelStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromARGB(255, 255, 255, 244)
+                    ),
+                  ),
+                  validator: (value){
+                    String pattern =
+                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                    RegExp regExp = RegExp(pattern);
+                    return regExp.hasMatch(value ?? '')
+                      ? null
+                      : 'El valor ingresado no es un correo';
+                  },
+                ),
+                TextFormField(
+                  autocorrect: false,
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: '***********',
+                    labelText: 'Contraseña',
+                    labelStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromARGB(255, 255, 255, 244)
+                    ),
+                  ),
+                  validator: (value){
+                    return (value != null && value.length >=8)
+                    ? null
+                    : 'La contrase;a debe ser mayor a 7 caracteres';
+                  },
+                )
+              ],
             ),
          ),
         ),
